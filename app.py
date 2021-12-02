1b6ede143c183b6819bfb85fb78e759312d97981
strike3test <strike3test@hotmail.com>
2021-12-03 00:18:53 +1300

    returns name from dict

--- app.py
+++ app.py
@@ -4,14 +4,17 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+names = {"taylor": {"age": 23, "gender": "female"},
+         "robin": {"age": 25, "gender": "male"}}
+
 class HelloWorld(Resource):
-    def get(self):
-        return {"data": "hello world!"}
+    def get(self, name):
+        return names[name]
 
-    def post(self):
-        return {"data": "posted"}
+    # def post(self):
+    #     return {"data": "posted"}
 
-api.add_resource(HelloWorld, "/helloworld")
+api.add_resource(HelloWorld, "/helloworld/<string:name>")
 
 if __name__ == "__main__":
-    app.run(debug=True) # only in development environment
+    app.run()
